d645405bc15f74f0d21948d473ee28d205dc8ca4
Kush Chakraborty <ks2d85@gmail.com>
2018-09-03 13:58:24 -0400

    added test file

--- cpp/CodeBlocksTest/brace_expand.cpp
+++ cpp/CodeBlocksTest/brace_expand.cpp
@@ -1,16 +1,20 @@
 #include <brace_expand.h>
 
+namespace {
+  const strvector EMPTY_VECTOR_FOR_ERRORS;
+}
+
 strvector BraceExpand::brace_expand(const std::string& input) {
   int start_loc = 0;
   strvector elements = element_or_expansion(input, start_loc, input.size(), /*outside_braces=*/true);
   if (elements.size() == 1 && elements[0].empty()) {
-    return strvector();
+    return EMPTY_VECTOR_FOR_ERRORS;
   }
   return elements;
 }
 
-strvector BraceExpand::element_or_expansion(const std::string& str, int& loc, const int end, bool outside_braces) {
-  strvector elements, suffixes;
+strvector BraceExpand::element_or_expansion(const std::string& str, int& loc, const int end, const bool outside_braces) {
+  strvector brace_elements, suffixes;
   std::string prefix;
   bool found_opening_brace = false, found_closing_brace = false;
   while (loc < end) {
@@ -21,13 +25,13 @@ strvector BraceExpand::element_or_expansion(const std::string& str, int& loc, co
     } else if (',' == curr) {
       if (outside_braces) {
         // commas invalid outside braces
-        return strvector();
+        return EMPTY_VECTOR_FOR_ERRORS;
       }
       // Finished processing this element.
       break;
     } else if ('{' == curr) {
       found_opening_brace = true;
-      elements = get_elements(str, ++loc, end);
+      brace_elements = get_brace_elements(str, ++loc, end);
     } else if ('}' == curr) {
       if (!found_opening_brace) {
         // DO not process this closing brace, leave it to the function invocation which encountered the opening brace.
@@ -36,37 +40,36 @@ strvector BraceExpand::element_or_expansion(const std::string& str, int& loc, co
       found_closing_brace = true;
       suffixes = element_or_expansion(str, ++loc, end, outside_braces);
       if (suffixes.empty()) {
-        // Invalid suffix found.
-        return strvector();
+        return EMPTY_VECTOR_FOR_ERRORS;
       }
       break;
     } else {
       // Unexpected character
-      return strvector();
+      return EMPTY_VECTOR_FOR_ERRORS;
     }
   }
   if (found_opening_brace && !found_closing_brace) {
-    return strvector();
+    return EMPTY_VECTOR_FOR_ERRORS;
   }
   strvector result;
-  for (const std::string& elem : elements) {
+  for (const std::string& elem : brace_elements) {
     for (const std::string& suffix : suffixes) {
       result.push_back(prefix + elem + suffix);
     }
   }
-  if (result.empty()) {
+  if (!found_opening_brace) {
+    // brace_elements would be empty.
     result.push_back(prefix);
   }
   return result;
 }
 
-strvector BraceExpand::get_elements(const std::string& str, int& loc, const int end) {
+strvector BraceExpand::get_brace_elements(const std::string& str, int& loc, const int end) {
   strvector elements;
   for (;loc < end; loc++) {
     strvector expansion = element_or_expansion(str, loc, end, /*outside_braces=*/false);
     if (expansion.empty() || (expansion.size() == 1 && expansion[0].empty())) {
-      // Invalid segment encountered.
-      return strvector();
+      return EMPTY_VECTOR_FOR_ERRORS;
     }
     elements.insert(elements.end(), expansion.begin(), expansion.end());
     if (',' == str[loc]) {
@@ -74,7 +77,7 @@ strvector BraceExpand::get_elements(const std::string& str, int& loc, const int 
      } else if ('}' == str[loc]) {
       break;
     } else {
-      return strvector();
+      return EMPTY_VECTOR_FOR_ERRORS;
     }
   }
   return elements;
--- cpp/CodeBlocksTest/brace_expand.h
+++ cpp/CodeBlocksTest/brace_expand.h
@@ -8,8 +8,8 @@ typedef std::vector<std::string> strvector;
 
 class BraceExpand {
 private:
-  strvector element_or_expansion(const std::string& str, int& loc, const int end, bool outer_element);
-  strvector get_elements(const std::string& str, int& loc, const int end);
+  strvector element_or_expansion(const std::string& str, int& loc, const int end, const bool outside_braces);
+  strvector get_brace_elements(const std::string& str, int& loc, const int end);
 public:
   strvector brace_expand(const std::string& input);
 };
