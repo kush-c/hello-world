cfebcade9d45d8e9132ecbf6b362523c20245331
Kush Chakraborty <ks2d85@gmail.com>
2018-09-03 14:06:39 -0400

    made the main tolerable, now to write up the test

--- cpp/CodeBlocksTest/main.cpp
+++ cpp/CodeBlocksTest/main.cpp
@@ -2,35 +2,14 @@
 #include <brace_expand.h>
 
 int main(int argc, char const *argv[]) {
-  strvector cases = {
-    "{A,B}",
-    "AB,C",
-    "AB,A{B,C{X,YZ}}D{E,F}",
-    "A{B,C{X,YZ}}D{E,F}",
-    "{A,B}{C,D}",
-    "{A,B{C,D}}",
-    "{ABC}",
-    "ABC",
-    "}ABC",
-    "X{ABC",
-    "{A,B,}",
-    "}{",
-    "{}",
-    "A,B,C",
-    "{A{B,C}",
-    "",
-    "A#B",
-    "A{B,C}D#"
-  };
+  if (argc < 2 ) {
+    return 1;
+  }
   BraceExpand b;
-  for (const auto& str : cases) {
-    std::cout << str << " -> ";
-    strvector expansion = b.brace_expand(str);
-    if (expansion.empty()) std::cout << "invalid input\n";
-    for (size_t i = 0; i < expansion.size(); ++i) {
-      const char suffix = i < (expansion.size() - 1) ? ' ' : '\n';
-      std::cout << expansion[i] << suffix;
-    }
+  strvector expansion = b.brace_expand(argv[1]);
+  for (size_t i = 0; i < expansion.size(); ++i) {
+    const char suffix = i < (expansion.size() - 1) ? ' ' : '\n';
+    std::cout << expansion[i] << suffix;
   }
   return 0;
 }
